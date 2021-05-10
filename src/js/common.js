199d60d1ff5b2e909c97f76557f04fba2ddfd2b8
stanislav.mokhyr <stanislav.mokhyr@idapgroup.com>
2021-05-10 18:53:43 +0300

    add animation

--- src/js/common.js
+++ src/js/common.js
@@ -1,11 +1,41 @@
 "use strict";
 
-const main_info = document.querySelector('.main-info');
-// const ellipse = [];
+const ellipseContainer = document.querySelectorAll('.ellipse-container');
+const mainWidth = 1300;
 
-for(let i = 0; i < 10; i++) {
-  let div = document.createElement("div");
-  div.classList.add('ellipse');
+ellipseContainer.forEach(containerElem => {
+  for(let i = 0; i < 30; i++) {
+    let ellipseElem = document.createElement("div");
+    ellipseElem.classList.add('ellipse');
 
-  main_info.append(div);
+    (i % 2 === 0) ? ellipseElem.classList.add('ellipse__max') : ellipseElem.classList.add('ellipse__min');
+
+    if(i % 3 === 0) {
+      ellipseElem.classList.add('ellipse__max--bg-yellow');
+    } else if(i % 4 === 0) {
+      ellipseElem.classList.add('ellipse__min--bg-yellow');
+    }
+
+    containerElem.append(ellipseElem);
+  }
+
+  const ellipse = document.querySelectorAll('.ellipse');
+  const elemWidth = mainWidth;
+  let elemHeight = containerElem.offsetHeight;
+
+  ellipse.forEach(item => {
+    let randomTop = getRandomHeight(0, elemHeight);
+    let randomLeft = getRandomWidth(0, elemWidth);
+
+    item.style.top = randomTop + 'px';
+    item.style.left = randomLeft + 'px';
+  });
+});
+
+function getRandomHeight(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getRandomWidth(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,15 @@
 import '../scss/main.scss';
 import '../index.html';
 import './common';
+import WOW from 'wow.js/dist/wow.js';
 
-console.log('Hello world');
+const wow = new WOW(
+  {
+    boxClass:     'wow',
+    offset:       100,
+    mobile:       false,
+  }
+)
+wow.init();
+
+// console.log('Hello world');
